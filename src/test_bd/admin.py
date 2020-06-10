c5b9b1173c15ab07fbc80135e60ad91f9e761adf
VeranikaKaraliova <nika-11@mail.ru>
2020-06-11 00:28:04 +0300

    'delete'

--- src/test_bd/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Parsing
-
-admin.site.register(Parsing)
-
-# Register your models here.
--- src/test_bd/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestBdConfig(AppConfig):
-    name = 'test_bd'
