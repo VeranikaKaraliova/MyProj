dc84aedd6cf3a7c72f84eaa67560881011bab75c
VeranikaKaraliova <nika-11@mail.ru>
2020-06-24 17:09:34 +0300

    log

--- src/profiles/auth.py
+++ src/profiles/auth.py
@@ -2,20 +2,20 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,
 
 
 class MyLogin(LoginView):
-    template_name ='registration/login.html'
+    template_name ='templates/registration/login.html'
     
 
 class MyLogout(LogoutView):
-    template_name = 'registration/logout.html'
+    template_name = 'templates/registration/logout.html'
 
 class MyPasswordChange(PasswordChangeView):
-    template_name ='registration/password_change.html'
+    template_name ='templates/registration/password_change.html'
 
 class MyPasswordReset(PasswordResetView):
-    template_name ='registration/password_reset_form.html'
+    template_name ='templates/registration/password_reset_form.html'
 
 class MyPasswordResetDone(PasswordResetDoneView):
-    template_name='registration/password_reset_done.html'
+    template_name='templates/registration/password_reset_done.html'
 
 class MyPasswordResetConfirm(PasswordResetConfirmView):
-    template_name='registration/password_onfirm.html'
+    template_name='templates/registration/password_onfirm.html'
