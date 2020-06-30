93d9cd1bfadc892f296b79068bd218abda8ee61b
VeranikaKaraliova <nika-11@mail.ru>
2020-06-30 23:21:14 +0300

    30/06/20

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -134,8 +134,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/home/VeranikaKaraliova/shop/MyProj/src/static/'
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = '/home/VeranikaKaraliova/shop/MyProj/src/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/VeranikaKaraliova/shop/MyProj/src/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
