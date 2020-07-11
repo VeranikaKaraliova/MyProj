4794f8afcdee504bd40cd652ee38addd14157944
VeranikaKaraliova <nika-11@mail.ru>
2020-07-11 09:08:47 +0300

    home-page

--- src/proj/urls.py
+++ src/proj/urls.py
@@ -20,14 +20,14 @@ from testgenre.views import CreateGenre, UpdateGenre, ListGenre, DeleteGenre
 from django.conf import settings
 from django.conf.urls.static import static
 #from profiles.auth import MyLogin, MyLogout
-
+from book.views import HomePage
 #from testgenre import urls as testgenre_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('create-genre/', CreateGenre.as_view(), name='create-genre'),
     #path('update-genre/<int:pk>', UpdateGenre.as_view(), name='update'),
-    #path('list-genre/', ListGenre.as_view(), name='list'),
+    path('', HomePage.as_view(), name='home-page'),
     #path('delete-genre/<int:pk>', DeleteGenre.as_view(), name='delete'),
     path('testgenre/', include('testgenre.urls', namespace='genres')),
     path('authors/', include('authors.urls', namespace='authors')),
